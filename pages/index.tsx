a0a160f30844f30468b87a6f78c5118b13054dc4
Adryan S <adryx7000@gmail.com>
2021-08-23 23:15:07 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return(
+    <div>
+      <h1>hello</h1>
+
+      <img src="img/tecnologias/javascript.png" alt="js" width="64px" height="64px" style={{borderRadius: "10px"}} />
+    </div>
+  );
+}
+
+export default Home;
