12eae6b8eb56d059ae9da660a00fe493f25f473e
adryan samuel <adryx7000@gmail.com>
2022-05-30 20:33:49 +0000

    new template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
 import BackgroundParticle from "src/components/background";
-import { Text } from "src/components/text";
-
 
 const Index: React.FC = () => {
   return (
     <>
       <BackgroundParticle/>
-
-      <Text />
     </>
   );
 };
--- src/components/background/index.tsx
+++ src/components/background/index.tsx
@@ -5,17 +5,9 @@ import { Container } from "./styles";
 
 const BackgroundParticle:React.FC = () => {
   const particlesInit = async (main) => {
-    console.log(main);
-
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
     await loadFull(main);
   };
 
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
   return (
     <Container>
 
