99d6e41726631f1fdf2a51bcb9a67c22fd607649
adryan samuel <adryx7000@gmail.com>
2022-06-01 20:18:38 +0000

    add text infos

--- src/components/text/socialLinks/index.tsx
+++ src/components/text/socialLinks/index.tsx
@@ -4,9 +4,9 @@ import {AiFigGithub, AiFigInstagram, AiFigLinkedin} from "src/icons";
 const SocialLinks = () => {
  return (
     <Container>
-        <div><a href="https://github.com/AdryanS" target="_blank"><AiFigGithub /></a></div>
-        <div className="margin"><a href="https://www.linkedin.com/in/adryan-samuel" target="_blank"><AiFigLinkedin /></a></div>
-        <div><a href="https://www.instagram.com/adx.adryan/" target="_blank"><AiFigInstagram /></a></div>
+        <div><a href="https://github.com/AdryanS" target="_blank" rel="noreferrer" ><AiFigGithub /></a></div>
+        <div className="margin"><a href="https://www.linkedin.com/in/adryan-samuel" target="_blank" rel="noreferrer"><AiFigLinkedin /></a></div>
+        <div><a href="https://www.instagram.com/adx.adryan/" target="_blank" rel="noreferrer"><AiFigInstagram /></a></div>
     </Container>
  )
 
