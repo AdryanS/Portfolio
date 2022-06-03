d45db8188d832d3c557415e7093f278c0054e66e
adryan samuel <adryx7000@gmail.com>
2022-06-03 22:17:38 +0000

    ”

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -5,7 +5,7 @@ export const Card = () => {
         <>
             <Container>
                 <h3>Sobre mim</h3>
-                <p>Me chamo Adryan Samuel, tenho 17 anos é sou de Brasília.</p>
+                <p>Me chamo Adryan Samuel, tenho 17 anos é moro em Brasília/DF.</p>
                 <p>Aprendi programação no começo de 2018, começando com HTML, CSS e JS desde então eu sou apaixonado em programação.</p>
                 <p>Gosto de jogar, criar objetos 3D, programar, criar games, criar interfaces e fazer minhas interfaces na web.</p>
                 <p>Amo criar interfaces bonitas e funcionais no figma, sou iniciante na area de UX/UI e pretendo aperfeiçoar essas habilidades .</p>
