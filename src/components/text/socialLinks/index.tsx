63b3c373b1c60af171594732f1ccedbcdee698f0
adryan samuel <70078649+AdryanS@users.noreply.github.com>
2022-12-27 14:45:06 -0300

    Update index.tsx

--- src/components/text/socialLinks/index.tsx
+++ src/components/text/socialLinks/index.tsx
@@ -6,7 +6,7 @@ const SocialLinks = () => {
     <Container>
         <div className="mgbt"><a href="https://github.com/AdryanS" target="_blank" rel="noreferrer" ><AiFigGithub /></a></div>
         <div className="margin mgbt"><a href="https://www.linkedin.com/in/adryan-samuel" target="_blank" rel="noreferrer"><AiFigLinkedin /></a></div>
-        <div className="mgbt"><a href="https://www.instagram.com/adx.adryan/" target="_blank" rel="noreferrer"><AiFigInstagram /></a></div>
+        <div className="mgbt"><a href="https://www.instagram.com/dev.adryan/" target="_blank" rel="noreferrer"><AiFigInstagram /></a></div>
     </Container>
  )
 
