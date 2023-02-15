acddf64ce0098d223ab84dffb75b80aac4ebc38b
adryan samuel <70078649+AdryanS@users.noreply.github.com>
2023-02-14 23:54:54 -0300

    Update index.tsx

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -5,10 +5,10 @@ export const Card = () => {
         <>
             <Container>
                     <h3>Sobre mim</h3>
-                    <p>Me chamo Adryan Samuel, tenho 17 anos é moro em Brasília/DF.</p>
-                    <p>Aprendi programação no começo de 2018, começando com HTML, CSS e JS desde então eu sou apaixonado em programação.</p>
-                    <p>Gosto de jogar, criar objetos 3D, programar, criar games, criar interfaces e fazer minhas interfaces na web.</p>
-                    <p>Amo criar interfaces bonitas e funcionais no figma, sou iniciante na area de UX/UI e pretendo aperfeiçoar essas habilidades .</p>
+                    <p>Olá, sou Adryan, 18 Anos, natural de Goianésia, Goiás, mas residente em Brasília-DF. <br /> Atuo como Desenvolvedor Full-Stack, com maior aptidão em Front-End desde 2020, onde 
+comecei a aprender ainda no Ensino Médio.</p>
+                    <p>Estou em busca de uma oportunidade em TI para ampliar horizontes, meu currículo 
+profissional e carreira como Desenvolvedor.</p>
                     {/* <button><p>ver mais</p></button> */}
             </Container>
         </>
