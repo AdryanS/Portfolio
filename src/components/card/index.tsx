8205934d971c3738455947c603d8a42167e09cd4
adryan samuel <70078649+AdryanS@users.noreply.github.com>
2023-02-14 23:57:19 -0300

    Update index.tsx

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -5,10 +5,9 @@ export const Card = () => {
         <>
             <Container>
                     <h3>Sobre mim</h3>
-                    <p>Olá, sou Adryan, 18 Anos, natural de Goianésia, Goiás, mas residente em Brasília-DF. <br /> Atuo como Desenvolvedor Full-Stack, com maior aptidão em Front-End desde 2020, onde 
-comecei a aprender ainda no Ensino Médio.</p>
-                    <p>Estou em busca de uma oportunidade em TI para ampliar horizontes, meu currículo 
-profissional e carreira como Desenvolvedor.</p>
+                    <p>Olá, sou Adryan, 18 Anos, natural de Goianésia, Goiás, mas residente em Brasília-DF.</p>
+                    <p> Atuo como Desenvolvedor Full-Stack, com maior aptidão em Front-End desde 2020, onde comecei a aprender ainda no Ensino Médio.</p>
+                    <p>Estou em busca de uma oportunidade em TI para ampliar horizontes, meu currículo profissional e carreira como Desenvolvedor.</p>
                     {/* <button><p>ver mais</p></button> */}
             </Container>
         </>
