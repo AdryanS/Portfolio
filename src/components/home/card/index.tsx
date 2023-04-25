3de80a6fe8ab7d866ee6d3938bfe6aeacc46da6a
Adryan <70078649+AdryanS@users.noreply.github.com>
2023-04-25 20:29:47 -0300

    Update index.tsx

--- src/components/home/card/index.tsx
+++ src/components/home/card/index.tsx
@@ -6,7 +6,7 @@ export const IndexCard = () => {
             <Container>
                     <h3>Sobre mim</h3>
                     <p>Olá, sou Adryan, 18 Anos, natural de Goianésia, Goiás, mas residente em Brasília-DF.</p>
-                    <p>Atuo como Desenvolvedor Full-Stack, com maior aptidão em Front-End desde 2020, onde comecei a aprender ainda no Ensino Médio.</p>
+                    <p>Atuo como Desenvolvedor Full-Stack, com maior aptidão em Front-End desde 2019, onde comecei a aprender ainda no Ensino Fundamental</p>
                     <p>Estou em busca de uma oportunidade em TI para ampliar horizontes, meu currículo profissional e carreira como Desenvolvedor.</p>
                     <a href="#Projetos"><p>Ver Projetos</p></a>
                     <a href="#Stack"><p>Ver Tecnologias</p></a>
